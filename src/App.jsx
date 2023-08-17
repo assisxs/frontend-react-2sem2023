1ccd65038d423a22d12616190cc0fe6049eea043
Raissa Assis <raissaassis017@gmail.com>
2023-08-17 15:55:25 -0300

    create layouts with components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Header from "./Header"
+import Sidebar from "./Sidebar"
+import Content from "./Content"
+import Footer from "./Footer"
+
+import "./App.css"
 
 const App = () => {
   return (
     <>
        <Header />
-       <div>
+       <div id="main">
          <Sidebar />
          <Content />
        </div>
