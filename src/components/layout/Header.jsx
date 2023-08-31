392518dc3bd91071a9ade0c5fc8bb72dd6f99c03
Raissa Assis <raissaassis017@gmail.com>
2023-08-31 15:02:26 -0300

    link update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,16 +1,17 @@
 import './Header.css'
+import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
     <header>
-        <h1>Coffee & Mountain</h1>
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Quem Somos</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-        </nav>
+      <h1>Coffee & Mountain</h1>
+      <nav>
+        <ul>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/quem-somos">Quem Somos</NavLink></li>
+          <li><NavLink to="/contato">Contato</NavLink></li>
+        </ul>
+      </nav>
     </header>
   )
 }
